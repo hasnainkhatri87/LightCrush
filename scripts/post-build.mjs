4bd88523f84666e566d6a6ea605caa3bb2e36475
Hasnain Khatri <hasnainkhatri87@example.com>
2026-06-01 18:49:02 +0500

    Fix Desktop Bundler for 100% Standalone EXEs

--- scripts/post-build.mjs
+++
@@ -1,6 +0,0 @@
-import fs from 'fs';
-
-if (fs.existsSync('.gitignore.backup')) {
-  fs.renameSync('.gitignore.backup', '.gitignore');
-  console.log('Restored .gitignore.');
-}
--- scripts/pre-build.mjs
+++
@@ -1,6 +0,0 @@
-import fs from 'fs';
-
-if (fs.existsSync('.gitignore')) {
-  fs.renameSync('.gitignore', '.gitignore.backup');
-  console.log('Temporarily renamed .gitignore to avoid Neutralino build exclusion.');
-}
